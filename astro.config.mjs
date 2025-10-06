aa4991980bb113dd3979ba6f2c3c70191ddfdf28
CodyRobards <cody.robards.95@gmail.com>
2025-10-05 19:56:36 -0500

    Add critical CSS integration and optimize styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
+import critters from '@astrojs/critters';
 
 // https://docs.astro.build/en/reference/configuration-reference/
-export default defineConfig({});
+export default defineConfig({
+  integrations: [critters({ preload: 'swap', pruneSource: true })],
+});
