6d44e6ca7ac66962e96c4e77836117db4da97287
CodyRobards <cody.robards.95@gmail.com>
2025-10-05 21:24:50 -0500

    Update ResponsiveImage component import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
 import critters from '@astrojs/critters';
-import image from '@astrojs/image';
 
 // https://docs.astro.build/en/reference/configuration-reference/
 export default defineConfig({
   integrations: [
     critters({ preload: 'swap', pruneSource: true }),
-    image(),
   ],
 });
